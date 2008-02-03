531cab2e940e5dffe6be2af27ec298e8629daffe
Callum Gibson <callumgibson@optusnet.com.au>
2008-02-03 11:13:44 +0000

    Change version number to 5.4.8-pre

--- version.c
+++ version.c
@@ -25,47 +25,5 @@
 /**    OR PERFORMANCE OF THIS SOFTWARE.                                     **/
 /*****************************************************************************/
 
-/* djhjr - 4/23/96
-char *Version = "Virtual Desktop TWM (VTWM) - R5 Release (5.3)";
-*/
-/* djhjr - 9/10/96
-char *Version = "Virtual Desktop TWM (VTWM) - R5 Release 5.4";
-*/
-/* djhjr - 3/12/97
-char *Version = "Virtual Desktop TWM (VTWM) - R5 Release 5.4.1";
-*/
-/* djhjr - 11/17/97
-char *Version = "Virtual Desktop TWM (VTWM) - R5 Release 5.4.2";
-*/
-/* djhjr - 1/1/98
-char *Version = "Virtual Desktop TWM (VTWM) - R5 Release 5.4.3";
-*/
-/* djhjr - 2/5/98
-char *Version = "Virtual Desktop TWM (VTWM) - X11R4-6 Release 5.4.4";
-*/
-/* djhjr - 3/14/98
-char *Version = "Virtual Desktop TWM (VTWM) - X11R4-6 Release 5.4.4a";
-*/
-/* djhjr - 3/20/98
-char *Version = "Virtual Desktop TWM (VTWM) - X11R4-6 Release 5.4.4b";
-*/
-/* djhjr - 8/11/98
-char *Version = "Virtual Desktop TWM (VTWM) - X11R4-6 Release 5.4.4c";
-*/
-/* djhjr - 9/27/98
-char *Version = "Virtual Desktop TWM (VTWM) - X11R4-6 Release 5.4.5";
-*/
-/* djhjr - 1/30/99
-char *Version = "Virtual Desktop TWM (VTWM) - X11R4-6 Release 5.4.5a";
-*/
-/* djhjr - 11/8/01
-char *Version = "Virtual Desktop TWM (VTWM) - X11R4-6 Release 5.4.6";
-*/
-/* djhjr - 10/1/04
-char *Version = "Virtual Desktop TWM (VTWM) - X11R4-6 Release 5.4.6a";
-*/
-/* djhjr - 10/2/04
-char *Version = "Virtual Desktop TWM (VTWM) - X11R4-6 Release 5.4.6b";
-*/
-char *Version = "Virtual Desktop TWM (VTWM) - X11R4-6 Release 5.4.7";
+char *Version = "Virtual Desktop TWM (VTWM) - X11R4-6 Release 5.4.8-pre";
 
